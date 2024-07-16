bb9cc755d267af40f4a8e765a654c4370612e375
lutfiangga <lutfiangga2908@gmail.com>
2024-07-16 19:39:52 +0700

    add contact

--- src/components/sections/contact.jsx
+++ src/components/sections/contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
 
     // Simpan kode untuk mengirim pesan ke WhatsApp di sini
     const whatsappMessage = `Nama: ${name} \n Subjek: ${subject} \n Pesan: ${message}`
-    const whatsappLink = `https://wa.me/6288xxxxxxxx?text=${encodeURIComponent(
+    const whatsappLink = `mailto:8193secure@yogirt.com?text=${encodeURIComponent(
       whatsappMessage
     )}`
 
